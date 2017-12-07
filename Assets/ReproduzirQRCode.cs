fc3e9078213f8b5542731e7615902ae3988838cb
lucas paulino <lucaspaulinosoares03@gmail.com>
2017-12-06 23:12:08 -0300

    Interface de QRCode

---
+++ Assets/ReproduzirQRCode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReproduzirQRCode : MonoBehaviour {
+
+	public GameObject imagemQRCode;
+	private Texture img; 
+
+	void Start () {
+		img = imagemQRCode.GetComponent<RawImage>().texture;
+		transform.GetChild (0).GetComponent<RawImage> ().texture = img;
+	}
+	
+
+	void Update () {
+		
+	}
+}
