e7008f76ebfa8a30fade94c1a2cd6d4198211ee7
BrunoPO <BrunoPO9896@gmail.com>
2018-03-15 08:39:52 -0300

    Added radarItem

---
+++ Assets/Scripts/Scene/itemRadar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class itemRadar : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.tag != "Player")
+			return;
+		if(col.name == "CeilingCheck" || col.name == "GroundCheck" || col.name == "Head" || col.name == "Target" )
+			col.transform.parent.transform.gameObject.GetComponent<EnemyAI>().targetSec = this.transform.parent.transform;
+		else
+			col.transform.gameObject.GetComponent<EnemyAI>().targetSec = this.transform.parent.transform;
+
+	}
+}
